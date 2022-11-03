a82a139fc13cb1b97939a5939d1baeb706f646d4
msdzilii <83410300+msdzilii@users.noreply.github.com>
2022-11-03 10:46:23 +0700

    landing Page User

--- app/Views/pages/User/landingPageUser.php
+++ app/Views/pages/User/landingPageUser.php
@@ -118,39 +118,20 @@
 				<div class="col-lg-3 col-md-6">
 					<div class="footer-box about-widget">
 						<h2 class="widget-title">About us</h2>
-						<p>Ut enim ad minim veniam perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-box get-in-touch">
-						<h2 class="widget-title">Get in Touch</h2>
-						<ul>
-							<li>34/8, East Hukupara, Gifirtok, Sadan.</li>
-							<li>support@fruitkha.com</li>
-							<li>+00 111 222 3333</li>
-						</ul>
+						<p>Sistem Informasi Penjualan Apotek adalah suatu kumpulan informasi yang 
+                    mendukung suatu proses pemenuhan kebutuhan suatu informasi yang bertanggung 
+                    jawab untuk menyediakan informasi penjualan obat dalam satu kesatuan proses yang 
+                    bertujuan untuk meningkatkan pelayanan pada Apotek</p>
 					</div>
 				</div>
 				<div class="col-lg-3 col-md-6">
 					<div class="footer-box pages">
 						<h2 class="widget-title">Pages</h2>
 						<ul>
-							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About</a></li>
-							<li><a href="services.html">Shop</a></li>
-							<li><a href="news.html">News</a></li>
-							<li><a href="contact.html">Contact</a></li>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Produk</a></li>
 						</ul>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<div class="footer-box subscribe">
-						<h2 class="widget-title">Subscribe</h2>
-						<p>Subscribe to our mailing list to get the latest updates.</p>
-						<form action="index.html">
-							<input type="email" placeholder="Email">
-							<button type="submit"><i class="fas fa-paper-plane"></i></button>
-						</form>
 					</div>
 				</div>
 			</div>
