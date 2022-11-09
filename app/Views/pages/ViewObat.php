cc1cccab22b8b33b8536458a06bc246a73383781
msdzilii <83410300+msdzilii@users.noreply.github.com>
2022-11-09 23:57:55 +0700

    edit tampilan admin

--- app/Views/pages/ViewObat.php
+++ app/Views/pages/ViewObat.php
@@ -14,43 +14,43 @@
     </div><!-- End Page Title -->
 
     <a href = "/create" class = "btn btn-primary mb-3 bi bi-plus-square-fill"> Tambah Obat </a>
-    <table class="table table-dark">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Nama Obat</th>
-                    <th scope="col">Harga Obat</th>
-                    <th scope="col">Stok Obat</th>
-                    <th scope="col">Deskripsi Obat</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                
-                <?php foreach ($obat as $obat):?>
-                <tr>
-                    <td><?php echo $obat['id_obat'];?></td><br>
-                    <td><?php echo $obat['nama_obat']; ?></td>
-                    <td><?php echo $obat['harga_obat']; ?></td>
-                    <td><?php echo $obat['stok_obat']; ?></td>
-                    <td><?php echo $obat['deskripsi_obat']; ?></td>
-                    <td>
-                        <div class="d-flex">
-                            <div class="box">
-                                <a class="btn btn-warning bi bi-pencil-fill mr-3 ml-3" href="/edit/<?= $obat['id_obat']?>"></a>
+    <div class="row p-3">
+        <table class="table">
+                <thead class="table-dark">
+                        <th scope="col">No</th>
+                        <th scope="col">Nama Obat</th>
+                        <th scope="col">Harga Obat</th>
+                        <th scope="col">Stok Obat</th>
+                        <th scope="col">Deskripsi Obat</th>
+                        <th scope="col">Action</th>
+                </thead>
+                <tbody>
+                    
+                    <?php foreach ($obat as $obat):?>
+                    <tr>
+                        <td><?php echo $obat['id_obat'];?></td><br>
+                        <td><?php echo $obat['nama_obat']; ?></td>
+                        <td><?php echo $obat['harga_obat']; ?></td>
+                        <td><?php echo $obat['stok_obat']; ?></td>
+                        <td><?php echo $obat['deskripsi_obat']; ?></td>
+                        <td>
+                            <div class="d-flex">
+                                <div class="box">
+                                    <a class="btn btn-warning bi bi-pencil-fill mr-3 ml-3" href="/edit/<?= $obat['id_obat']?>"></a>
+                                </div>
+                                <div class="box">
+                                    <form action = "/delete/<?= $obat['id_obat']?>" method="post">
+                                        <input name="_method" value="DELETE" type="hidden">
+                                        <button type="submit" class="btn btn-danger bi bi-trash3-fill mr-3 ml-3"></button>
+                                    </form>
+                                </div>
                             </div>
-                            <div class="box">
-                                <form action = "/delete/<?= $obat['id_obat']?>" method="post">
-                                    <input name="_method" value="DELETE" type="hidden">
-                                    <button type="submit" class="btn btn-danger bi bi-trash3-fill mr-3 ml-3"></button>
-                                </form>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
     </main>
 
 <?= $this -> endSection() ?>
